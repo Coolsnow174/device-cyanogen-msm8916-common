13ddf9a510b1034537125f360e09d85f88cde494
Coolsnow174 <coolsnow174@gmail.com>
2026-07-12 09:35:14 +0000

    msm8916-common: Fix ZRAM not activating on boot
    - Move mkswap/swapon from init.rc 'exec' to init.qcom.mem.sh service
      (exec often fails due to SELinux domain transition)
    - Fix swapon priority to 32767
    - Add user root/group root to qcom-mem service for zram0 access

--- rootdir/bin/init.qcom.mem.sh
+++ rootdir/bin/init.qcom.mem.sh
@@ -1,4 +1,9 @@
 #!/vendor/bin/sh
+# Zram setup
+/system/bin/mkswap /dev/block/zram0
+/system/bin/swapon /dev/block/zram0 -p 32767
+echo 100 > /proc/sys/vm/swappiness
+
 # Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
